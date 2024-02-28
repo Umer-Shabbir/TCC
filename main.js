5627c9eab419be51df6032b53b071471341a2c6b
umer123-webdeveloper <mumershabbir40@gmail.com>
2024-02-29 01:54:47 +0500

    Made some changes to the home pahe like the about us section and created the bootcamps page

---
+++ main.js
@@ -0,0 +1,6 @@
+var htmlBootcamp = document.getElementById('html-bootcamp');
+let bootcampDetailIntro = document.getElementById('bootcamp-detail-intro');
+
+htmlBootcamp.addEventListener('click', function () {
+  bootcampDetailIntro.textContent = "Html"
+},false);
